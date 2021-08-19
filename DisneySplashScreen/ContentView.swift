589544490d1b502ca478fda2e892b5fdb0f5a3c3
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-19 22:33:48 +0200

    Initial Commit

---
+++ DisneySplashScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DisneySplashScreen
+//
+//  Created by Michele Manniello on 19/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DisneySplashScreen/DisneySplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneySplashScreenApp.swift
+//  DisneySplashScreen
+//
+//  Created by Michele Manniello on 19/08/21.
+//
+
+import SwiftUI
+
+@main
+struct DisneySplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
